413a0784e0435537ff4e89bab7978895d5ab1acb
yapyx <yapyx317@gmail.com>
2026-05-10 01:11:00 +0800

    update notifications

---
+++ flutter_app/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,20 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:record_web/record_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
